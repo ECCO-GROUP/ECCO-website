5b32979b82e5f2fb8457f4707d5210c2a1211ba5
Anil Natha <Anil.Natha@jpl.nasa.gov>
2018-01-31 17:14:18 -0800

    Updated template for all products page.

--- products/all/index.php
+++ products/all/index.php
@@ -19,7 +19,38 @@ EOF;
 			</header>
 			<div class='wysiwyg_content'>
 
-				
+				<p>ECCO products listed below are freely available through several types of servers (a direct link is provided below via each product's `Release Name' in the product table). We kindly ask users to refer to the citable identifier associated with the product they choose to use (provided below via the `Release identifier' links). The products are listed below in an order that reflects their time of publication (see `Release identifier'). The following products are currently being extended: ECCO-v4 (JPL/AER/MIT), SOSE (UCSD), ECCO2 (JPL), ECCO-JPL (JPL), and GECCO2 (UH).</p>
+				<p>ECCO products are primarily released in the form of monthly field time series. These fields are primarily distributed on their native model grid to allow for accurate transport and budget computations. However fields interpolated to a simple longitude latitude grid may also be provided, which can most easily be plugged into third party software. Some of the products further provide the associated obsevational inputs, which allow users to e.g. assess model-data misfits. Examples of these supplementary data sets are indicated by a '>' under the associated `Release identifier'.</p>
+				<p>For comments or questions please concact us via: <a href="mailto:ecco-support@mit.edu">ecco-support@mit.edu</a></p>
+
+				<div>
+					<table class="full-width one-quarter-width">
+						<thead>
+							<th class="text-left">Product</th>
+							<th>Release Identifier</th>
+							<th>Release Name</th>
+							<th>Covered Time Period</th>
+							<th>Model Grid</th>
+							<th>Depth Levels</th>
+							<th>Estimation Method</th>
+							<th>Software Generation</th>
+							<th>Comments</th>
+						</thead>
+						<tbody>
+							<tr>
+								<td class="text-left">VALUE</td>
+								<td>VALUE</td>
+								<td>VALUE</td>
+								<td>VALUE</td>
+								<td>VALUE</td>
+								<td>VALUE</td>
+								<td>VALUE</td>
+								<td>VALUE</td>
+								<td>VALUE</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
 
 			</div><!-- //.wysiwyg_content -->
 			
