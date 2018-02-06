d6caff43acdfbbcb6165290c092dd5fb306c637f
Luis Espinoza <luis.a.espinoza@jpl.nasa.gov>
2018-02-05 17:16:21 -0800

    updated homepage

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ EOF;
         <section class='clearfix main_feature'>
 		  <div class='master-slider ms-skin-light-3' id='masterslider'>
 		  	<!-- Slide 1 (regular template) -->
-			<article class='dark_background ms-slide slide-4' data-delay='15' data-fill-mode='fill'>
+			<article class='dark_background ms-slide slide-4' data-delay='6' data-fill-mode='fill'>
 			  <div class='gradient_container_top'></div>
 			  <img alt='slide4 background' data-src='/assets/images/home/u5.jpg' src='/assets/images/home/u5.jpg'>
 			  <div class='bg_dark bottom_right floating_text_area ms-layer' data-duration='300' data-ease='easeOutQuart' data-effect='right(90)' data-resize='false' data-type='text'>
@@ -39,7 +39,7 @@ EOF;
 			<!-- End Slide 1 -->
 
 			<!-- Slide 2 (regular template) -->
-			<article class='dark_background ms-slide slide-2' data-delay='15' data-fill-mode='fill'>
+			<article class='dark_background ms-slide slide-2' data-delay='6' data-fill-mode='fill'>
 			  <div class='gradient_container_top'></div>
 			  <img alt='slide2 background' data-src='/assets/images/home/v4r3_available_online.jpg' src='/assets/images/home/v4r3_available_online.jpg'>
 			  <div class='bg_dark bottom_right floating_text_area ms-layer' data-duration='300' data-ease='easeOutQuart' data-effect='right(90)' data-resize='false' data-type='text'>
@@ -60,7 +60,7 @@ EOF;
 			<!-- End Slide 2 -->
 			
 			<!-- Slide 3 (regular template) -->
-			<article class='dark_background ms-slide slide-3' data-delay='15' data-fill-mode='fill'>
+			<article class='dark_background ms-slide slide-3' data-delay='6' data-fill-mode='fill'>
 			  <div class='gradient_container_top'></div>
 			  <img alt='slide3 background' data-src='/assets/images/home/Pie17carousel.jpg' src='/assets/images/home/Pie17carousel.jpg'>
 			  <div class='bg_dark bottom_left floating_text_area ms-layer extended' data-duration='300' data-ease='easeOutQuart' data-effect='right(90)' data-resize='false' data-type='text'>
@@ -75,12 +75,10 @@ EOF;
 			  <div class='gradient_container_bottom'></div>
 			</article>
 			<!-- End Slide 3 -->
-
 			
 		  </div>
 		</section>
 	  </section>
-
 
 	  <div class='scrollblock'>
 		<div class='loading'></div>
@@ -90,11 +88,9 @@ EOF;
           </a><div class='arrow_masks'></div><div class='arrow_mask'></div><div class='arrow_masks'></div>
         </div>
       
-		
-		  <div class="grid_layout">
+		<div class="grid_layout">
 			<p>The “Estimating the Circulation and Climate of the Ocean” (ECCO) consortium is directed at making the best possible estimates of ocean circulation and its role in climate. Solutions are obtained by combining state-of-the-art ocean circulation models with nearly complete global ocean data sets in a physically and statistically consistent manner.  Products are being utilized to understanding ocean variability, biological cycles, coastal physics, and geodesy, and are available for general applications.</p>
-		  </div><!-- //.grid_layout -->
-
+		</div><!-- //.grid_layout -->
 
 		<div class="module">
 			<div class="grid_layout">
@@ -226,7 +222,7 @@ EOF;
 				<br />
 				<div class="grid text-center">
 					<div class="grid-cell grid--1of4">
-				 		<a href="/products/">
+				 		<a href="/products/all/">
 				 			<div class="hover-item">
 				 				<div class="hover-container">
 				 					<img alt="Products" title="Products" src="/assets/images/home/u17.jpg">
@@ -270,7 +266,7 @@ EOF;
 				 				<div class="hover-container">
 				 					<img alt="Reasearch" title="Research" src="/assets/images/home/u12.jpg">
 					 				<div class="description">
-					 					Click hereot review publications and presentations.
+					 					Click here to review publications and presentations.
 					 				</div>
 					 			</div>
 				 				<div class="content_title">Research</div>
@@ -284,59 +280,45 @@ EOF;
 		<div class="module">
 			<div class="grid_layout">
 				<h2 class="content_title">Latest Updates</h2>
-				<br />
-				<div class="grid">
-					<div class="grid-cell grid--3of4">
-						<section class="grid_gallery list_view">
-							<div class="grid_layout">
-								<ul class="articles">
-									<li class='slide'>
-										<a href=''>
-											<div class='image_and_description_container'>
-												<div class='img'><img alt="ECCO Tutorial at Ocean Sciences 2018" title="ECCO Tutorial at Ocean Sciences 2018" src="/assets/images/home/workshop.jpg"></div>
-												<div class='list_text_content'>
-													<div class='content_title no-pad'>
-														ECCO Tutorial at Ocean Sciences 2018
-													</div>
-													<div class='article_teaser_body'>
-														<p>The ECCO project will hold a workshop during Ocean Sciences 2018 in Portland, OR. More details regarding the workshop's agenda to follow.<br />
-														Title: ECCO Workshop: How to use the latest ECCO Ocean State Estimate <br />
-														Date and Time: Thursday, February 15, 2018: 12:45 PM - 1:45 PM <br />
-														Location: Oregon Convention Center, F152 </p>
-													</div>
-												</div>
-											</div>
-										</a>
-									</li>
+				<section class="grid_gallery list_view">					
+					<ul class="articles">
+						<li class='slide'>
+							<a href=''>
+								<div class='image_and_description_container'>
+									<div class='img'><img alt="ECCO Tutorial at Ocean Sciences 2018" title="ECCO Tutorial at Ocean Sciences 2018" src="/assets/images/home/workshop.jpg"></div>
+									<div class='list_text_content'>
+										<div class='content_title no-pad'>
+											ECCO Tutorial at Ocean Sciences 2018
+										</div>
+										<div class='article_teaser_body'>
+											<p>The ECCO project will hold a workshop during Ocean Sciences 2018 in Portland, OR. More details regarding the workshop's agenda to follow.<br />
+											Title: ECCO Workshop: How to use the latest ECCO Ocean State Estimate <br />
+											Date and Time: Thursday, February 15, 2018: 12:45 PM - 1:45 PM <br />
+											Location: Oregon Convention Center, F152 </p>
+										</div>
+									</div>
+								</div>
+							</a>
+						</li>
 
-									<li class='slide'>
-										<a href=''>
-											<div class='image_and_description_container'>
-												<div class='img'><img alt="ECCO version 4 release 3" title="ECCO version 4 release 3" src="/assets/images/home/v4r3.jpg"></div>
-												<div class='list_text_content'>
-													<div class='content_title no-pad'>
-														ECCO version 4 release 3, covering 1992-2015, now available online
-													</div>
-													<div class='article_teaser_body'>
-														The new release 3 extends the Version 4 estimate using additional observations. The product also incorporates improvements in modeling and estimation. A summary document describes details of the changes. Also available are descriptions of how to evaluate property budgets and how to reproduce the results and generate additional fields using MITgcm.. 
-													</div>
-												</div>
-											</div>
-										</a>
-									</li>
-								</ul>
-							</div>
-						</section>
-					</div>
-					<div class="grid-cell grid--1of4">
-						<div class="border pad-20">
-							ECCO Demo
-							<p><strong>Dec 29</strong> | <a href="" target="_blank">Add to calendar</a></p>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					  		tempor incididunt ut labore et dolore magna aliqua.
-					  	</div>
-					</div>
-			</div>
+						<li class='slide'>
+							<a href=''>
+								<div class='image_and_description_container'>
+									<div class='img'><img alt="ECCO version 4 release 3" title="ECCO version 4 release 3" src="/assets/images/home/v4r3.jpg"></div>
+									<div class='list_text_content'>
+										<div class='content_title no-pad'>
+											ECCO version 4 release 3, covering 1992-2015, now available online
+										</div>
+										<div class='article_teaser_body'>
+											The new release 3 extends the Version 4 estimate using additional observations. The product also incorporates improvements in modeling and estimation. A summary document describes details of the changes. Also available are descriptions of how to evaluate property budgets and how to reproduce the results and generate additional fields using MITgcm.. 
+										</div>
+									</div>
+								</div>
+							</a>
+						</li>
+					</ul>
+				</section><!-- //.grid_gallery -->
+			</div><!-- //.grid_layout -->
 		</div><!-- //.module -->
 
 	</div><!-- //.scrollblock -->
