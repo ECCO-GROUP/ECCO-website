a4a0f75d0296cbcf53cab6d64aa68201e252a265
Anil Natha <Anil.Natha@jpl.nasa.gov>
2018-02-05 16:06:43 -0800

    Added empty about page and updated header and footer to link to it.

---
+++ about/index.php
@@ -0,0 +1,32 @@
+<?php
+	/* define page vars */
+	$include_path = '../';
+	$page_title = "ECCO | About ";
+	$page_id = 'research';
+	$theme = 'light_background'; //dark_background || light_background
+	$include_other = <<<EOF
+EOF;
+
+	include_once($include_path . 'includes/header.inc.php');
+?>
+	<!-- START CONTENT -->
+	<div class='header_mask'></div>
+
+	<section class='content_page module white-bg'>
+		<div class='grid_layout'>
+			<header id='page_header'>
+				<h1 class='article_title'>About</h1>
+			</header>
+			<div class='wysiwyg_content'>
+
+				
+
+			</div><!-- //.wysiwyg_content -->
+			
+			</div><!-- //.grid -->
+		</div><!-- //.grid_layout -->
+	</section><!-- //.content_page -->
+
+	<!-- END CONTENT -->
+<?php include_once('../../includes/footer.inc.php'); ?>
+</html>
