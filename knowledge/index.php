95f5684b41611b51f4d2926308885a32df7869f0
Jon Nelson <Jon.D.Nelson@jpl.nasa.gov>
2018-02-13 08:07:46 -0800

    Update index.php

--- knowledge/index.php
+++ knowledge/index.php
@@ -38,7 +38,7 @@
                         </p>
                         <div class="accordion">
                             <div class="accordion-section pad-20">
-                                Automatic Differentiation (AD) is a technology for automatically augmenting computer programs, including arbitrarily complex simulations, with statements for the computation of derivatives (tangent linear, adjoint, Hessian, etc.), also known as sensitivities. AD tools in our context provide source-to-source transformation of a function, given as computer code, to generate efficient and accurate (truncation-free) code for computing first, second and higher-order derivatives of the given function.
+                                Automatic Differentiation (AD) is a technology for automatically augmenting computer programs, including arbitrarily complex simulations, with statements for the computation of derivatives (tangent linear, adjoint, Hessian, etc.), also known as sensitivities. In ECCO, the adjoint model is obtained by an AD tool. AD tools in our context provide source-to-source transformation of a function, given as computer code, to generate efficient and accurate (truncation-free) code for computing first, second and higher-order derivatives of the given function.
                                 <!-- <br /><br /><a href="automatic-differentiation/">&rsaquo; Read more</a>-->
                             </div><!-- //.accordion-section -->
                         </div><!-- //.accordion -->
@@ -71,7 +71,7 @@
                                 sequentially in time; e.g., model state at time "n" is corrected,
                                 followed by that at time "n+1". Filters correct models using
                                 observations formally in the past; smoothers use observations both
-                                formally in the past as well as the future. A Kalman filter and
+                                formally in the future as well as the past. A Kalman filter and
                                 Rauch-Tung-Striebel (RTS) smoother are particular forms of these
                                 recursive least-squares estimators.
                                 <!--<br /><br /><a href="optimization/">&rsaquo; Read more</a>-->
@@ -107,7 +107,7 @@
                         </div><!-- //.accordion -->
                         
                          <p>
-                            <span>What are the issues with Closing Budgets?</span>
+                            <span>What are issues with Closing Budgets?</span>
                         </p>
                         <div class="accordion">
                             <div class="accordion-section pad-20">
@@ -120,7 +120,8 @@
                                 <br /><br />
                                 Smoothed solutions, in comparison, generally do satisfy model
                                 constraints as they include estimates of model error sources consistent
-                                with corrections to the state.
+                                with corrections to the state. Among ECCO products, those using either the adjoint method or
+   the RTS smoother are such smoothed solutions that allow budget closures.
                                 <!--<br /><br /><a href="kalman-filter-and-smoother/">&rsaquo; Read more</a>-->
                             </div><!-- //.accordion-section -->
                         </div><!-- //.accordion -->
