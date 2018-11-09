3a154649f1f6785a3fb4fd1fed25436ce396bb3e
Ichiro Fukumori <34175004+ifukumori@users.noreply.github.com>
2018-11-08 16:34:46 -0800

    Update index.php

--- research/presentations/index.php
+++ research/presentations/index.php
@@ -1,7 +1,7 @@
 <?php
 	/* define page vars */
 	$include_path = '../../';
-	$page_title = "ECCO | Research | Presentations";
+	$page_title = "ECCO | Presentations";
 	$page_id = 'research';
 	$theme = 'light_background'; //dark_background || light_background
 	$include_other = <<<EOF
