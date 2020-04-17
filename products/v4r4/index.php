7fcd5ad24a6c79654dae6b57f01b79fc745739a5
ifenty <ifenty@gmail.com>
2020-04-17 15:06:49 -0700

    v4r4 product page edits

--- products/v4r4/index.php
+++ products/v4r4/index.php
@@ -22,31 +22,59 @@ EOF;
 			</header>
 			<div class='wysiwyg_content'>
 
-				<h2>ECCO-V4r4</h2>
+				<h2>ECCO Central Produciton Version 4 Release 4</h2>
 				<small>Authors: Fukumori, Ichiro; Wang, Ou; Fenty, Ian; Forget, Gael; Heimbach, Patrick; Ponte, Rui M.</small>
 
 				<div class="grid">
 					<div class="grid-cell flexit">
 						
 						<h4>Description:</h4>
-						<p>Version 4 Release 4 (V4r4), covering the period 1992-2017, represents
-ECCO's latest ocean state estimate. This product is an updated edition to that described by Forget et al. (2015, Geosci. Model Dev.). Version 4 is the first multi-decadal ECCO estimate that is truly global, including the Arctic Ocean. The Release 4 edition includes improvements in time-period (1992-2017), model (e.g., sea-ice), observations (e.g., GRACE, Aquarius), and constraints (e.g., correlated errors).
+						<p>Version 4 Release 4 (V4r4), covering the period 1992-2017, represents ECCO's latest ocean state estimate. This product is an updated edition to that described by Forget et al. (2015, Geosci. Model Dev.). Version 4 is the first multi-decadal ECCO estimate that is truly global, including the Arctic Ocean. The Release 4 edition includes improvements in time-period (1992-2017), model (e.g., sea-ice), observations (e.g., GRACE, Aquarius), and constraints (e.g., correlated errors).
 </p>
 						<p>[<i>Image at right</i>] Global mean time-series of sea level and ocean bottom pressure (in equivalent sea level) of V4r4 in comparison to observations.</p>
-                                                <h4>ECCO Drive:</h4>
+
+<!-- Start  of Ian Fenty 2020-04-17 -->
+
+				<h2>Citation</h2>
+
+				<p>As a condition of using this product, you <strong><i>must</i></strong> using the following citations:</p>
+				<div class="indent gray-bg pad-20">
+					<i>
+						<p>ECCO Consortium, 2019: ECCO Central Estimate (Version 4 Release 4). Retrieved from [to be specified by user]</p>
+      <br>
+						<p>Forget, G., J.-M. Campin, P. Heimbach, C. N. Hill, R. M. Ponte, and C. Wunsch, 2015: ECCO version 4: an integrated framework for non-linear inverse modeling and global ocean state estimation. Geoscientific Model Development, 8, 3071-3104, doi:10.5194/gmd-8-3071-2015</p>
+					</i>
+				</div>
+
+    <p>A synopsis document is available here:<br>
+       Fukumori, I., O. Wang, I. Fenty, G. Forget, P. Heimbach, and R. M. Ponte, 2019: ECCO Version 4 Release 4, Available at <a href="https://ecco.jpl.nasa.gov/drive/files/Version4/Release4/doc/v4r4_synopsis.pdf" target="_blank" rel="noopener noreferrer">https://ecco.jpl.nasa.gov/drive/files/Version4/Release4/doc/v4r4_synopsis.pdf</a></p>
+
+
+     <h2>Download Dataset</h2>
+
+         <p><i>Note: ECCO's anonymous ftp server ftp://ecco.jpl.nas.gov has been discontinued.</i></p>
+
+         <h4>Option 1: Monthly fields hosted on ECCO Drive</h4>
+
 <!-- Ou Wang 2019-12-09 -->
-                                                <p><strong>Due to NASA's mandate to disallow the use of the ftp protocol for data access, ECCO's anonymous ftp server ftp://ecco.jpl.nasa.gov has been replaced by a WebDAV server called ECCO Drive. Each user must first register for an <a href="https://urs.earthdata.nasa.gov/docu    mentation">Earthdata account</a> at <a href="https://urs.earthdata.nasa.gov/users/new">https://urs.earthdata.nasa    .gov/users/new</a> in order to access the ECCO products. ECCO Drive <a href="https://ecco.jpl.nasa.gov/drive/files">https://ecco.jpl.nasa.gov/drive/files</a> offers a familiar interface for users to browse and download data through their browser. Additionaly, ECCO's WebDAV interface allows one to connect to ECCO as if it were a local drive. It also allows users to access data via a command line interface, enabling scripted data extracting. A detailed help page can be found at <a href="https://ecco.jpl.nasa.gov/drive/help">https://ecco.jpl.nasa.gov/drive/help</a>. </strong></p>
-                                                <h4>Command line example:</h4>
-                                                <p>To use wget to download ECCO v4r4's monthly SSH on the native grid: </p>
-                                                <p><strong>wget -r --no-parent --user YOUREARTHDATAUSERNAME --ask-password https://ecco.jpl.nasa.gov/drive/files/Version4/Release4/nctiles_monthly/SSH</strong></p>
-<!-- End of Ou Wang 2019-12-09 -->
-                                                <p>When prompted for password, you need to enter your ECCO Drive's WebDAV password, not your Earthdata account's password. To get the WebDAV password, log in with your Earthdata account from a web browser to <a href="https://ecco.jpl.nasa.gov/drive">https://ecco.jpl.nasa.gov/drive</a>. Once successfully log in, you will be re-directed to the page of your WebDAV/Programmatic API credentials. Your ECCO Drive's WebDAV password is in the second box from top. This is the password that you would use for the wget command. </p>
-						<br />
-<!-- Ou Wang 2019-12-09 -->
-						<p><strong><a href="https://ecco.jpl.nasa.gov/drive/files/Version4/Release4/" class="button white">Download Product (excl. daily fields) </a></strong></p>
-						<p><strong><a href="https://data.nas.nasa.gov/ecco/data.php?dir=/eccodata/llc_90/ECCOv4/Release4" class="button white">Download Daily Fields</a></strong></p>
-<!-- End of Ou Wang 2019-12-09 -->
-						<p><strong><a href="https://web.corral.tacc.utexas.edu/OceanProjects/ECCO/ECCOv4/Release4/" class="button white">Download Product (mirror)</a></strong></p>
+
+             <p>ECCO Drive <a href="https://ecco.jpl.nasa.gov/drive/files">https://ecco.jpl.nasa.gov/drive/files</a> is a WebDAV server that offers users a familiar interface to browse and download data through their browser. Additionaly, ECCO's WebDAV interface allows one to connect to ECCO as if it were a local drive. It also allows users to access data via a command line interface, enabling scripted data extracting.</p>
+
+             <p><strong> Users must first register for an <a href="https://urs.earthdata.nasa.gov/documentation">Earthdata account</a> at <a href="https://urs.earthdata.nasa.gov/users/new">https://urs.earthdata.nasa.gov/users/new</a></strong></p>.
+
+            <p>Instructions for remote mounting of the ECCO Drive can be found at <a href="https://ecco.jpl.nasa.gov/drive/help">https://ecco.jpl.nasa.gov/drive/help</a>.<br>
+               Instructions for downloading files from the ECCO Drive using the <i>wget</i> command line program are available here: <a href="https://drive.google.com/open?id=1XqQsfV1KnEX_ejQkcJoNrv6Qxknt8Kcr">Using wget to download multiple files and directories from ECCO drive.pdf</a>.
+            </p>
+
+            <strong><a href=https://ecco.jpl.nasa.gov/drive/files/" class="button white">Download Monthly Fields from the ECCO Drive</a></strong>
+
+         <h4>Option 2: Daily Fields hosted on the NASA Advanced Supercomputing (NAS) ECCO Data Portal</h4>
+
+             The <a href=https://data.nas.nasa.gov/>NAS Data Portal</a> hosts daily ECCO fields on the <a href="https://data.nas.nasa.gov/ecco/data.php?dir=/eccodata/llc_90/ECCOv4/Release4">NAS ECCO Data Portal</a>
+
+            <strong><a href="https://data.nas.nasa.gov/ecco/data.php?dir=/eccodata/llc_90/ECCOv4/Release4" class="button white">Download Daily Fields from the NAS ECCO Data Portal</a></strong>
+
+<!-- End of Ian Fenty 2020-04-17 -->
 					</div>
 					<div class="grid-cell grid--1of3 margin-20">
 						<div><img src="/assets/images/home/v4r4/v4r4_gmsl_gmobp.png"></div>
@@ -103,15 +131,7 @@ ECCO's latest ocean state estimate. This product is an updated edition to that d
 					</table>
 				</div>
 
-				<h2>Citation</h2>
-				<p>As a condition of using this product, you <strong><i>must</i></strong> cite the use of this product using the following citation.</p>
-				<div class="indent gray-bg pad-20">
-					<i>
-						<p>Fukumori, I., O. Wang, I. Fenty, G. Forget, P. Heimbach, and R. M. Ponte, 2019: ECCO Version 4 Release 4, Avfailable at <a href="https://ecco.jpl.nasa.gov/drive/files/Version4/Release4/doc/v4r4_synopsis.pdf" target="_blank" rel="noopener noreferrer">https://ecco.jpl.nasa.gov/drive/files/Version4/Release4/doc/v4r4_synopsis.pdf</a></p>
-						<p>Forget, G., J.-M. Campin, P. Heimbach, C. N. Hill, R. M. Ponte, and C. Wunsch, 2015: ECCO version 4: an integrated framework for non-linear inverse modeling and global ocean state estimation. Geoscientific Model Development, 8, 3071-3104, doi:10.5194/gmd-8-3071-2015</p>
-					</i>
-				</div>
-				<p>For more information, see our <a href="https://www.jpl.nasa.gov/imagepolicy/">Use and Copyright Web page</a>.</p>
+
 
 				<h2>User Guide</h2>
 				<p>A detailed description of available files and their content are summarized in a "User Guide" in the link below.  Although written for the previous release (V4r3), the User Guide, in addition to other files that folllow, apply equally to the present Release 4 estimate. 
